39261e30894f670294710546501939769b4bdd94
PacoValdes01 <paco_valdes_ugarte@hotmail.com>
2024-03-14 14:58:37 -0600

    Agregar archivos de proyecto.

---
+++ SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Xml.Linq;
+using System.Numerics;
+
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class AccesoController : Controller
+    {
+        private readonly IUsuarioService _usuarioServicio;
+
+        public AccesoController(IUsuarioService usuarioServicio)
+        {
+            _usuarioServicio = usuarioServicio;
+        }
+
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if (claimUser.Identity.IsAuthenticated) {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public IActionResult RestablecerClave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(VMUsuarioLogin modelo)
+        {
+            if (modelo.Correo==null)
+            {
+                ViewData["Mensaje"] = "No se ha introducido correo, intente nuevamente.";
+                return View();
+            }
+            if (modelo.Clave == null)
+            {
+                ViewData["Mensaje"] = "No se a introducido contraseña, intente nuevamente.";
+                return View();
+            }
+
+            Usuario usuario_encontrado = await _usuarioServicio.ObtenerPorCredenciales(modelo.Correo, modelo.Clave);
+
+            if (usuario_encontrado == null)
+            {
+                ViewData["Mensaje"] = "El correo o contraseña, no son correctos, favor de intentar nuevamente, gracias.";
+                return View();
+            }
+
+            if (usuario_encontrado.EsActivo == false)
+            {
+                ViewData["Mensaje"] = "El usuario está desactivado, favor de acudir con su administrador del sistema, gracias.";
+                return View();
+            }
+
+            ViewData["Mensaje"] = null;
+
+            List<Claim> claims = new List<Claim>() { 
+                new Claim(ClaimTypes.Name, usuario_encontrado.Nombre),
+                new Claim(ClaimTypes.NameIdentifier, usuario_encontrado.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Role, usuario_encontrado.IdRol.ToString()),
+                new Claim("UrlFoto", usuario_encontrado.UrlFoto),
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties properties = new AuthenticationProperties() { 
+                AllowRefresh = true,
+                IsPersistent = modelo.MantenerSesion,
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                properties
+                );
+
+            return RedirectToAction("Index","Home");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RestablecerClave(VMUsuarioLogin modelo)
+        {
+            try
+            {
+                string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/RestablecerClave?clave=[clave]";
+
+                bool resultado = await _usuarioServicio.RestablecerClave(modelo.Correo, urlPlantillaCorreo);
+
+                if (resultado)
+                {
+                    ViewData["Mensaje"] = "Proceso Existoso. Su contraseña fue restablecida, revise su correo.";
+                    ViewData["MensajeError"] = null;
+                }
+                else
+                {
+                    ViewData["Mensaje"] = null;
+                    ViewData["MensajeError"] = "UPS, algo salió mal. Favor de intentarlo más tarde.";
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewData["Mensaje"] = null;
+                ViewData["MensajeError"] = ex.Message;
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ SistemaVenta.AplicacionWeb/Utilidades/ViewComponents/MenuUsuarioViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace SistemaVenta.AplicacionWeb.Utilidades.ViewComponents
+{
+    public class MenuUsuarioViewComponent : ViewComponent   
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            string nombreUsuario = "";
+            string urlFotoUsurio = "";
+
+            if (claimUser.Identity.IsAuthenticated)
+            {
+                nombreUsuario = claimUser.Claims
+                    .Where(c => c.Type == ClaimTypes.Name)
+                    .Select(c => c.Value).SingleOrDefault();
+
+                urlFotoUsurio = ((ClaimsIdentity)claimUser.Identity).FindFirst("urlFoto").Value;
+            }
+
+            ViewData["nombreUsuario"] = nombreUsuario;
+            ViewData["urlFotoUsuario"] = urlFotoUsurio;
+
+            return View();
+        }
+    }
+}
